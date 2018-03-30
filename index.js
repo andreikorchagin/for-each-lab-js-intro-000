fe8ba89b3a186948694747911261a3186a49025c
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-03-30 21:55:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function iterativeLog(array){
-  array.forEach(function(index, element){
+function iterativeLog(array) {
+  array.forEach(function(index, element) {
     console.log(`${index}: ${element}`);
   });
 }
-function iterate(callback{
-  array = ['a','b','c','d'];
-  array.forEach(callback());
-  return array;
-}
-function doToArray(array, callback{
-  array.forEach(callback());
-}
+
+function iterate(callback) {
+    array = ['a', 'b', 'c', 'd'];
+    array.forEach(callback());
+    return array;
+  }
+
+  function doToArray(array, callback) {
+      array.forEach(callback());
+    }
